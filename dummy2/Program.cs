7d766407f8029d5ec97db2d6a56256ab12db78b0
mahesh <lokilokesh1680@gmail.com>
2023-03-09 12:31:21 +0530

    lokesh add: dummy2 folder is added

---
+++ dummy2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace dummy2
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to dummy2");
+        }
+    }
+}
